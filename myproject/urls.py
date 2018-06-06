43de2bdb638b9a6a16d78d58835e8e64b3c12ca9
Paolo Cozzi <bunop@libero.it>
2018-06-06 16:33:13 +0200

    Reusing pagination template

--- myproject/urls.py
+++ myproject/urls.py
@@ -61,7 +61,7 @@ urlpatterns = [
         name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$',
-        views.topic_posts,
+        views.PostListView.as_view(),
         name='topic_posts'),
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$',
         views.reply_topic,
